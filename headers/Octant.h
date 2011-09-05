2e2cddc9fab65657f89770b41851414cb19bac03
M.Cid.Clan@gmail.com <M.Cid.Clan@gmail.com@4a8f34f7-0c43-9fca-786f-65bd1bdd1506>
2011-09-05 15:08:02 +0000

    This version is under development: Currently, a new space partitionning using octree is being developped.

---
+++ headers/Octant.h
@@ -0,0 +1,22 @@
+/*
+ * Cloud of voxels (COV) project
+ * Author: mcidclan, m.cid.clan@gmail.com
+ * Date: 2011
+ */
+
+#ifndef OCTANT_H
+#define OCTANT_H
+
+
+	class Octant
+	{
+		public:
+			static Vec3sui curpos;
+
+		private:
+			Octant ***children;
+	};
+
+#endif
+
+
--- headers/math.h
+++ headers/math.h
@@ -1,6 +1,6 @@
 /*
  * Cloud of voxels (COV) project
- * Author: by mcidclan. m.cid.clan@gmail.com
+ * Author: mcidclan. m.cid.clan@gmail.com
  * Date: 2011
  */
 
@@ -42,6 +42,33 @@
 			v2->y += v1.y;
 			v2->z += v1.z;	
 		}
+
+
+		/*
+		 * vecadd
+		 */
+		template<typename T1, typename T2>
+		void vecsub(const T1 v1, T2 *v2)
+		{
+			v2->x -= v1.x;
+			v2->y -= v1.y;
+			v2->z -= v1.z;	
+		}
+
+
+		/*
+		 * vecsub
+		 */
+		/*template<typename T1, typename T2, typename T3>
+		T3 vecsub(const T1 v1, const T2 v2)
+		{
+			T3 v3 =
+			{
+				(T3)(v2->x - v1.x);
+				(T3)(v2->y - v1.y);
+				(T3)(v2->z - v1.z);
+			}
+		}*/
 
 
 		/*
